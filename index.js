2d9bf8c93806a542b08c2040aee6e43a36f356e0
Daria Skoryk <dariaskoryk@gmail.com>
2023-09-29 22:56:44 +0200

    new task

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const goodbye = (name) => {
 };
 
 goodbye('Pavel Ovesny');
+
+const fillSubject = (subject) => {
+  const subjectElement = document.querySelector('.email__subject');
+  subjectElement.textContent = subject;
+};
+fillSubject('new notification');
+
+const fillBody = (body, name) => {
+  const emailElement = document.querySelector('.email__body');
+  emailElement.textContent = body;
+  goodbye(name);
+};
+fillBody(
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, doloremque ipsa fugiat quibusdam placeat consectetur nesciunt sed? Voluptas, totam consequuntur minima tempore quaerat quasi repellendus, corrupti maiores est autem mollitia?',
+  'Jan Novak',
+);
